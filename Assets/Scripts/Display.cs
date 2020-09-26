4d1780992357d6224f6c0324264dd6c877bf52dd
MikalWhaley <mwhaley@csumb.edu>
2020-09-25 20:10:23 -0700

    first commit

---
+++ Assets/Scripts/Display.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+using UnityEngine.UIElements;
+
+public class Display : MonoBehaviour
+{
+    public TextMeshProUGUI score;
+    public TextMeshProUGUI time;
+    public TextMeshProUGUI coins;
+
+    private float timeLimit = 999;
+    private int coinCount = 0;
+    private int scoreCount = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeLimit -= (Time.deltaTime);
+        time.text = "TIME \n" + (timeLimit.ToString()).Substring(0, 3);
+        ;
+    }
+}
---
+++ Assets/Scripts/QuestionMark.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class QuestionMark : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Material myQmaterial;
+    public float amplify = 150;
+    private float yValue = 1;
+    void Start()
+    {
+        myQmaterial = GetComponent<Renderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        yValue += 1 * amplify * Time.deltaTime;
+
+        if(yValue > 20)
+        {
+            myQmaterial.mainTextureOffset = new Vector2(1, (myQmaterial.mainTextureOffset.y + .2f) % 1);
+            yValue = 0;
+        }
+
+        float y = Time.deltaTime * ((myQmaterial.mainTextureOffset.y + .2f) % 1);
+
+        
+    }
+}
